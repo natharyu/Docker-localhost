6a90b6588cfc06ea99079d7c7c7d912e7aef1a27
Natharyu <anthony.dewittelebrun@gmail.com>
2020-12-20 17:33:42 +0100

    Add Database and adminer to webserver

--- webserver/nginx/www/index.php
+++ webserver/nginx/www/index.php
@@ -1,2 +1,13 @@
+
+
+<?php
+$dbconn = pg_connect('host=db port=5432 dbname=webserver_db user=user1 password=password')
+    or die('Could not connect');
+     
+echo '<pre>' . var_export(pg_version($dbconn), true) . '</pre>';
+
+pg_close($dbconn);
+?>
+
 <?php
 echo phpinfo();
